d5ddc77d0ca1e01b9627a58146e8a0305b726148
Andrew Krause <aek.krause@gmail.com>
2021-10-12 11:38:55 -0500

    Updated user login information.

--- app.js
+++ app.js
@@ -15,16 +15,19 @@ require('dotenv').config();
 
 // Require packages for the server that were installed.
 const express = require("express");
+const flash = require('connect-flash'); // --> MAY NOT NEED THIS
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+var user_ID = mongoose.Types.ObjectId();
 
 // Require additional security packages for login information
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const LocalStrategy = require("passport-local");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate"); // --> If code directly above does not work, run this code.
+const findOrCreate = require("mongoose-findorcreate");
 
 // Create web app using express and set view engine to EJS
 const app = express();
@@ -41,6 +44,9 @@ app.use(session({
     saveUninitialized: false
 }));
 
+// Use the flash module.
+app.use(flash());
+
 // Initialize the passport package. Use passport in the session.
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,23 +58,22 @@ app.use(passport.session());
 
 // Set up a connection to the database using mongoose.
 mongoose.connect("mongodb://localhost:27017/aptivDB", {useNewUrlParser: true, useUnifiedTopology: true});
-//mongoose.set('useCreateIndex', true); //--> DEPRECATED
 
 // Create a mongoose schema (blueprint) for the users in the database.
 const userSchema = new mongoose.Schema({
-    userID: Number, // --> May not need this
+    userID: String, // --> May not need this
     firstName: String,
     lastName: String,
-    email: String,
+    username: String, // <-- USED FOR GOOGLE OAUTH; this is your EMAIL ADDRESS! FIGURE OUT HOW TO GET IT
     password: String,
     googleId: String,
-    status: String,
+    //status: String, // <-- NEED TO SET STATUS; worry about this later!!!
     userEvents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Event'}]
 });
 
 // Create a mongoose schema for the programs in the database.
 const programSchema = new mongoose.Schema({
-    programID: Number, // -->  May not need this
+    programID: String, // -->  May not need this
     programName: String,
     programDescription: String,
     programEvents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Event'}] // --> Reference to the event model. Can be many events.
@@ -76,9 +81,9 @@ const programSchema = new mongoose.Schema({
 
 // Create a mongoose schema for the events in the database.
 const eventSchema = new mongoose.Schema({
-    eventID: Number, // --> May not need this
+    eventID: String, // --> May not need this
     eventName: String,
-    eventDate: {type : Date, default: Date.now }, // --> Will need to change this later.
+    eventDate: {type: Date, default: Date.now }, // --> Will need to change this later.
     eventTime: Number,
     eventProgram: {type: mongoose.Schema.Types.ObjectId, ref: 'Program'} // --> Each unique event is only associated with one program.
 });
@@ -93,6 +98,11 @@ const UserModel = new mongoose.model("User", userSchema);
 const ProgramModel = new mongoose.model("Program", programSchema);
 const EventModel = new mongoose.model("Event", eventSchema);
 
+// ============================================================================================================
+
+
+/* SECTION: AUTHENTICATE USERS AND USE GOOGLE OAUTH */
+
 // Set up passport-local-mongoose configurations.
 passport.use(UserModel.createStrategy());
 
@@ -104,7 +114,8 @@ passport.serializeUser(function(user, done){
 // Deserialize user.
 passport.deserializeUser(function(id, done){
     UserModel.findById(id, function(err, user){
-        done(err, user);
+        //done(err, user);
+        done(null, user);
     });
 });
 
@@ -114,12 +125,12 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID, 
     clientSecret: process.env.CLIENT_SECRET, 
     callbackURL: "http://localhost:3000/auth/google/team-aptiv", 
-    // userProfileURL: "https//www.googleapis.com/oauth2/v3/userinfo" // <-- If deprecation, update this.
+    //profileFields: ['id', 'displayName', 'email']
     },
     function(accessToken, refreshToken, profile, cb) {
-        // console.log(profile); // <-- Comment this out when done testing.
-
-        UserModel.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(profile); // <-- Comment this out when done testing.
+                                                                                                    // --> USERNAME NEEDS TO BE THE EMAIL ADDRESS!
+        UserModel.findOrCreate({ googleId: profile.id, firstName: profile.name.givenName, lastName: profile.name.familyName, username: profile.id}, function (err, user) {
             return cb(err, user);
         });
     }
@@ -132,7 +143,7 @@ passport.use(new GoogleStrategy({
 
 // Default route, which is the home page.
 app.get("/", function(req, res){
-
+    req.flash('message', 'Success!!')
     // Render the home page and determine if user is undefined.
     res.render("home", {
         user: req.user
@@ -172,6 +183,17 @@ app.get("/events", function(req, res){
 app.get("/login", function(req, res){
     res.render("login");
 });
+
+
+
+// --> Try implementing flash methods
+// app.get('/login', function(req,res) {
+// res.render('login', {message: req.flash('message')});
+// });
+
+// <%= message %>
+
+
 
 // Create a route for viewing the register page.
 app.get("/register", function(req, res){
@@ -242,13 +264,6 @@ app.get("/logout", function(req, res){
 //     }
 // });
 
-// // Create a route for going back to the home page when the user
-// // clicks the "logout" button. Deauthenticate user.
-// app.get("/logout", function(req, res){
-//     req.logout();
-//     res.redirect("/");
-// });
-
 // ============================================================================================================
 
 
@@ -274,8 +289,21 @@ app.post("/home", function(req, res){
 // Create a post request for when user clicks the "Register" button.
 app.post("/register", function(req, res){
 
+    // Create a new user ID for the developers to use and track. // <-- MAYBE!!!
+    user_ID = mongoose.Types.ObjectId();
+
+    // Create a new instance of the user schema 
+    // to pass into the register method.
+    const newUser = new UserModel({
+        userID: user_ID,
+        firstName: req.body.fname,
+        lastName: req.body.lname,
+        username: req.body.username
+      });
+
     // Obtain the user information and, if no errors, redirect new user to profile page.
-    UserModel.register({fname: req.body.fname, lname: req.body.lname, username: req.body.username}, req.body.password, function(err, user){
+    // User info such as first and last name obtained by storing it in an object, newUser.
+    UserModel.register(newUser, req.body.password, function(err, user){
         if(err) {
             console.log(err);
             res.redirect("/register");
@@ -291,15 +319,13 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res){
     
     // Enable the user to login. 
-    const user = new UserModel({
-        fname: req.body.fname, // --> MAY NOT NEED THIS
-        lname: req.body.lname, // --> MAY NOT NEED THIS
+    const userLogin = new UserModel({
         username: req.body.username,
         password: req.body.password
     });
-
+            // CHECK IF YOU CAN SET IT UP SO THAT IT ONLY CHECKS IF USERNAME AND PASSWORDS EXIST IN DATABASE FOR THAT USER???
     // Check if the user is in our database. Check if user has entered the correct credentials.
-    req.login(user, function(err){ // --> NEED TO CREATE A CASE TO HANDLE WHEN USER ENTERS IN INCORRECT INFO.
+    req.login(userLogin, function(err){ // --> NEED TO CREATE A CASE TO HANDLE WHEN USER ENTERS IN INCORRECT INFO.
         if(err) {                   // --> NEED TO CREATE A MESSAGE FOR THE USER WHEN THEY LOG OUT OF THEIR ACCOUNT.
             console.log(err);
             res.redirect("/home"); // --> REDIRECT TO A LOGIN PAGE THAT HAS A MESSAGE THAT TELLS THE USER THEIR LOGIN FAILED.
@@ -311,6 +337,45 @@ app.post("/login", function(req, res){
     });
 });
 
+
+
+// // --> Check if user entered correct information.
+// app.post('/login',
+//   passport.authenticate('local', { successRedirect: '/',
+//                                    failureRedirect: '/login',
+//                                    failureFlash: true })
+// );
+
+// // --> Check if user entered correct information.
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     UserModel.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+// // --> Check if user entered correct information.
+// app.get('/login', function(req, res, next) {
+//     passport.authenticate('local', function(err, user, info) {
+//       if (err) { return next(err); }
+//       if (!user) { return res.status(401).send({"ok": false}); }
+//       req.logIn(user, function(err) {
+//         if (err) { return next(err); }
+//         return return res.send({"ok": true});
+//       });
+//     })(req, res, next);
+//   });
+
+
+  
 // Create a post request for when the user clicks the "Logout" button.
 app.post("/logout", function(req, res){
     req.logout();
