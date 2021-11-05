cc784cc656d143731eb480abadb714688ba9d468
Andrew Krause <aek.krause@gmail.com>
2021-11-05 07:02:19 -0500

    Created donations page

---
+++ javascript/temporary.js
@@ -0,0 +1,14 @@
+/**
+ * May add behaviors in this file
+ * for various functionalities later.
+ * 
+ * Project: Aptiv Web Application
+ * Authors: Andrew Krause, Riley Radle, 
+ * Anthony Musbach, Zach Gephart
+ * Class: Software Design IV (CS 341)
+ * Group: #2
+ * Date: 12/15/2021
+ *
+ */
+
+// ADD CODE HERE MAYBE...
